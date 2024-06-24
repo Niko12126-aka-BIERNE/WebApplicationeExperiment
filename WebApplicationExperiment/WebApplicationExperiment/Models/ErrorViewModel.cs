100ad8b30e26aef469d736ee52ceadf79a96df79
Niko12126-aka-BIERNE <64725654+Niko12126-aka-BIERNE@users.noreply.github.com>
2024-06-24 11:36:19 +0200

    Initial commit

---
+++ WebApplicationExperiment/WebApplicationExperiment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationExperiment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
